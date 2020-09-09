8cd24ac78060eb02291055599ccc4ffecb22748c
Chen Lu <blacksea.chen@hotmail.com>
2020-09-09 19:32:28 +0800

    to break pipeline

--- src/test/java/length/test.java
+++ src/test/java/length/test.java
@@ -3,11 +3,13 @@ package length;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class test {
 
   @Test
   public void testAdd() {
     assertEquals(42, Integer.sum(19, 23));
+    assertTrue(false);
   }
 }
